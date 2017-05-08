9d51937944ee371f6371b05cd9820933387cb823
Martin <s65179@beuth-hochschule.de>
2017-05-08 15:12:33 +0200

    A1 a)

--- Ueb2/QED.c
+++ Ueb2/QED.c
@@ -1,8 +1,42 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+
+
+#define CHILD 0
+#define FAILURE -1
 
 int main() {
-	printf("PID: %u\n", getpid());
-	printf("PPID: %u\n", getppid());
-return 0;
+	int i=0;
+
+	for(i=0; i<1; i++) {
+
+		switch( fork() ) {
+
+			case FAILURE:
+				perror("Fork failed.\n");
+				break;
+
+			case CHILD:
+				printf("Child - PID: %u, PPID: %u\n\n", getpid(), getppid());
+				fflush(stdout);
+				break;
+
+			default:
+				printf("Parent - PID: %u, PPID: %u\n\n", getpid(), getppid());
+				fflush(stdout);
+				printf("Child: -PID: %u, PPID: %u\n\n", getpid(), getppid());
+				sleep(1);	
+				exit(0);
+				
+				break;
+		}
+	}
+
+
+
+	return 0;
 }	
